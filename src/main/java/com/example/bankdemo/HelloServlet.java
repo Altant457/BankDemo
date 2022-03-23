ac42539aba9e7afb433d0a72a7ca77ddb2a91659
Marcus <28458922+Altant457@users.noreply.github.com>
2022-03-23 13:27:40 +0100

    Accounts are listed via jsp

--- src/main/java/com/example/bankdemo/HelloServlet.java
+++ src/main/java/com/example/bankdemo/HelloServlet.java
@@ -1,19 +1,50 @@
 package com.example.bankdemo;
 
+import DomainObjects.Account;
+import sun.reflect.generics.tree.Tree;
+
 import java.io.*;
+import java.util.Map;
+import java.util.TreeMap;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
-    private String message;
+
+    Map<String, Account> accountMap = new TreeMap<>();
 
     public void init() {
+        Account acc1 = new Account("Markus", "1", 100);
+        Account acc2 = new Account("Sebastian", "1", 100);
+        Account acc3 = new Account("Timi", "1", 100);
+        Account acc4 = new Account("March", "1", 100);
+        Account acc5 = new Account("Patrick", "1", 100);
+        Account acc6 = new Account("Otto", "1", 100);
+        Account acc7 = new Account("Marie", "1", 100);
+        Account acc8 = new Account("Nicki", "1", 100);
+        Account acc9 = new Account("Micki", "1", 100);
+        Account acc10 = new Account("Nikolaj", "1", 100);
 
+        accountMap.put(acc1.getName(), acc1);
+        accountMap.put(acc2.getName(), acc2);
+        accountMap.put(acc3.getName(), acc3);
+        accountMap.put(acc4.getName(), acc4);
+        accountMap.put(acc5.getName(), acc5);
+        accountMap.put(acc6.getName(), acc6);
+        accountMap.put(acc7.getName(), acc7);
+        accountMap.put(acc8.getName(), acc8);
+        accountMap.put(acc9.getName(), acc9);
+        accountMap.put(acc10.getName(), acc10);
+
+        ServletContext servletContext = getServletContext();
+        servletContext.setAttribute("accounts", accountMap);
     }
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        request.getRequestDispatcher("index.jsp").forward(request, response);
     }
 
     public void destroy() {
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,4 +1,5 @@
 <%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
 <html>
     <head>
@@ -6,6 +7,10 @@
     </head>
     <body>
         <h1><%= "Hello World!" %></h1><br />
-        <a href="hello-servlet">Hello Servlet</a>
+        <a href="hello-servlet">Hent konto liste</a>
+        <h1>Liste af konti</h1>
+        <c:forEach items="${applicationScope.accounts}" var="account">
+            ${account.value.name} : ${account.value.saldo} kr. <br />
+        </c:forEach>
     </body>
 </html>
